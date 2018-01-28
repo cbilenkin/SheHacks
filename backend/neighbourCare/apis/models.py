c99383948aba54b43aafae5f278d2a9825708114
shubhi31 <mittal.sh@husky.neu.edu>
2018-01-28 01:16:30 -0500

    Added models

--- backend/neighbourCare/apis/models.py
+++ backend/neighbourCare/apis/models.py
@@ -8,12 +8,17 @@ class LocationSync(models.Model):
     zipcode = models.IntegerField()
     sync_time = models.DateTimeField()
 
+    def __str__(self):
+        return str(self.zipcode)
+
 class Device(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     phone = models.IntegerField(max_length=10)
     email = models.EmailField()
-    location = models.OneToOneField(
+    location = models.ForeignKey(
         LocationSync,
         on_delete=models.CASCADE,
     )
+    def __str__(self):
+        return str(self.phone)
