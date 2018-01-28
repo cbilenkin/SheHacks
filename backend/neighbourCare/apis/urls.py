f13e7d320b02a5f98f184911cc2c72b0fd479b5e
shubhi31 <mittal.sh@husky.neu.edu>
2018-01-28 00:12:47 -0500

    backend listing api

--- backend/neighbourCare/apis/urls.py
+++ backend/neighbourCare/apis/urls.py
@@ -8,5 +8,6 @@ router.register(r'devices', views.DeviceViewSet)
 
 urlpatterns = [
     url(r'^', include(router.urls)),
+    #url('list/^?P<zipcode>.+)/$', views.DeviceList.as_view()),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
--- backend/neighbourCare/apis/views.py
+++ backend/neighbourCare/apis/views.py
@@ -2,6 +2,7 @@ from .models import Device
 from .models import LocationSync
 from rest_framework import viewsets
 from rest_framework import serializers
+from rest_framework import generics
 
 
 class LocationSerializer(serializers.ModelSerializer):
@@ -21,3 +22,31 @@ class DeviceViewSet(viewsets.ModelViewSet):
     """
     queryset = Device.objects.all()
     serializer_class = DeviceSerializer
+
+    def get_queryset(self):
+        """
+        Optionally restricts the returned purchases to a given user,
+        by filtering against a `username` query parameter in the URL.
+        """
+        #import ipdb; ipdb.set_trace();
+        queryset = Device.objects.all()
+        zipcode = self.request.query_params.get('zipcode', None)
+        if zipcode is not None:
+            queryset = queryset.filter(location__zipcode=zipcode)
+        return queryset
+
+
+class DeviceList(generics.ListAPIView):
+    serializer_class = DeviceSerializer
+
+    def get_queryset(self):
+        """
+        Optionally restricts the returned purchases to a given user,
+        by filtering against a `username` query parameter in the URL.
+        """
+        import ipdb; ipdb.set_trace();
+        queryset = device.objects.all()
+        zipcode = self.request.query_params.get('zipcode', None)
+        if zipcode is not None:
+            queryset = queryset.filter(location__zipcode=zipcode)
+        return queryset
